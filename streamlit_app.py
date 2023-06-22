9b3deb9109ccbcb01a3b1b5eca8121bee6826362
RichardLemieux-E08478 <93401913+RichardLemieux-E08478@users.noreply.github.com>
2023-06-22 11:30:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
         #import requests
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
         # Normalize the json response 
